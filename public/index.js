81efac2c34c996169dd4c18ddffbc177c3a2cd70
James Magallanes <fridaysyckness@gmail.com>
2023-07-29 13:30:32 +0800

    Update list.html

--- public/index.js
+++ public/index.js
@@ -1,22 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-      // Fetch and display the Firestore data using JavaScript
-    const dataList = document.getElementById("dataList");
-
-    fetch('/list-data')
-    .then((response) => response.json())
-    .then((data) => {
+    // Use HTMX to update the data after a successful request
+    htmx.on('htmx:afterRequest', (event) => {
+      if (event.detail.elt.id === 'dataList') {
+        const dataList = document.getElementById('dataList');
+        const data = JSON.parse(event.detail.xhr.response);
         const dataListHTML = data.map((item) => {
-            if (item.status === "redacted") {
-                // Hide the content for items with "redacted" status
-                return `<div class="list-item"><span class="guide">#${item.guide}.</span> <span class="item-redacted"></span></div>`;
-              } else {
-                return `<div class="list-item"><span class="guide">#${item.guide}.</span> <span><span class="name">${item.name}</span><span class="dash">&ndash;</span><span class="status">${item.status}</span></span></div>`;
-              }}).join('');
+          if (item.status === "redacted") {
+            return `<div class="list-item"><span class="guide">#${item.guide}.</span> <span class="item-redacted"></span></div>`;
+          } else {
+            return `<div class="list-item"><span class="guide">#${item.guide}.</span> <span><span class="name">${item.name}</span><span class="dash">&ndash;</span><span class="status">${item.status}</span></span></div>`;
+          }
+        }).join('');
         dataList.innerHTML = dataListHTML;
-    })
-    .catch((error) => {
-        console.error(error);
-        dataList.innerHTML = `<div class='list-item'>Error fetching data</div>`;
+      }
     });
-});
-    
+  });
