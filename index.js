75583d40e5ef80f88672230d0f2f8347e13daa2a
James Magallanes <fridaysyckness@gmail.com>
2023-07-29 10:17:47 +0800

    clean struc
    rem caching, mx files, +styling

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 })
 
+app.get('/list', (req, res) => {
+    res.sendFile('list.html', { root: path.join(__dirname, 'public') });
+});
+
 const cors = require('cors');
 app.use(cors());
 
@@ -23,58 +27,26 @@ admin.initializeApp({
 // Get a reference to the Firestore database
 const db = admin.firestore();
 
-const cache = {};
+// w/o Cache
+// New route to fetch data as JSON
+app.get('/list-data', (req, res) => {
+  db.collection('the-blacklist')
+    .get()
+    .then((snapshot) => {
+      const data = [];
+      snapshot.forEach((doc) => data.push(doc.data()));
 
-// Caching middleware function
-function cacheMiddleware(req, res, next) {
-    const key = req.originalUrl || req.url;
-    
-    if (cache[key] && !isDataStale(cache[key])) {
-      // If data is present in the cache, send it as the response      
-      res.setHeader('Content-Type', 'text/html');
-      res.send(cache[key].html);
-    } else {
-      // If data is not in the cache, proceed to fetch data from Firestore
-      db.collection('the-blacklist')
-        .get()
-        .then((snapshot) => {
-          const data = [];
-          snapshot.forEach((doc) => data.push(doc.data()));
-
-          // Convert data to HTML list
-          const dataListHTML = data.map((item) => `<li>${item.name}</li>`).join('');
-
-          // Store data in the cache with an expiration time of 10 minutes (10 * 60 * 1000 milliseconds)
-          cache[key] = {
-            html: `<ul>${dataListHTML}</ul>`,
-            timestamp: Date.now(),
-          };
-
-          // Respond with the HTML list    
-          res.setHeader('Content-Type', 'text/html');
-          res.send(cache[key].html);
-        })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).json({ error: 'Something went wrong' });
-        });
-    }
-  }
-
-  function isDataStale(cachedData) {
-    const currentTime = Date.now();
-    const cachedTime = cachedData.timestamp;
-    //const duration = 10 * 60 * 1000; // 10 minutes in milliseconds
-    const duration = 60 * 1000; // 1 minute in milliseconds
-    return currentTime - cachedTime >= duration;
-  }
-
-app.get('/list', cacheMiddleware);
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: 'Something went wrong' });
+    });
+});
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
 
 module.exports = app;
