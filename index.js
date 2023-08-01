6813fedf59122fbd8c28cf35bce4c58ad394b7d6
James Magallanes <fridaysyckness@gmail.com>
2023-08-01 21:29:54 +0800

    Update routes
    set cc

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const admin = require('./config/firebase-cfg');
+const cacheDuration = 15552000;
 
 dotenv.config();
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
+
+const setCacheControl = (req, res, next) => {
+  res.setHeader('Cache-Control', `s-maxage=${cacheDuration}`);
+  next();
+};
+app.use(setCacheControl);
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', {root: path.join(__dirname, 'public')});
@@ -18,6 +25,14 @@ const listRouter = require('./routes/listRouter')
 app.use("/list", listRouter)
 app.get('/list', (req, res) => {
   res.render('list/index');
+});
+
+app.get('/list/v2', (req, res) => {
+  res.render('list/v2/index');
+});
+
+app.get('/list/version1/:status', (req, res) => {
+  res.render('list/status');
 });
 
 const port = 3000;
--- routes/listRouter.js
+++ routes/listRouter.js
@@ -14,7 +14,7 @@ if (!admin.apps.length) {
 }
 
 const db = admin.firestore();
-router.get('/v1', (req, res) => {
+router.get('/version1', (req, res) => {
     db.collection(collectionName)
       .where('v1', '>=', 0)
       .where('v1', '<=', 200)
@@ -38,4 +38,60 @@ router.get('/v1', (req, res) => {
     });
 });
 
+router.get('/version2', (req, res) => {
+  db.collection(collectionName)
+    .where('v2', '>=', 0)
+    .where('v2', '<=', 200)
+    .orderBy('v2')
+    .get()
+    .then((snapshot) => {
+      const data = [];
+      snapshot.forEach((doc) => {
+        const docData = doc.data();
+        data.push({
+          id: doc.id,
+          ...docData
+        });
+      });
+      res.json(data);
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: 'Something went wrong' });
+  });
+});
+
+router.get('/version1/:status', async (req, res) => {
+  try {
+    const status = req.params.status.toLowerCase(); // Get the status from the URL and convert to lowercase
+
+    // Query Firestore to get the user data with the specified status
+    const snapshot = await db.collection(collectionName)
+      .where('v1', '>=', 0)
+      .where('v1', '<=', 200)
+      .where('status', '==', status)
+      .limit(5)
+      .orderBy('v1')
+      .get();
+
+    const data = [];
+    snapshot.forEach((doc) => {
+      const docData = doc.data();
+      console.log(data);
+
+      data.push({
+        id: doc.id,
+        ...docData
+      });
+    });
+
+    // Send the user data as a JSON response
+    res.json(data);
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
 module.exports = router
